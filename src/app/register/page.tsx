9354ea11f1e009738555638ace594d2bc01cd078
hernan789 <hernanlduarte@hotmail.com>
2024-01-19 15:39:58 -0300

    agrego registe-create-account

---
+++ src/app/register/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CreateAccount from "../../components/CreateAccount";
+
+const CreateAccountPage: React.FC = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-[#3d1df3]">
+      <CreateAccount />
+    </div>
+  );
+};
+
+export default CreateAccountPage;
