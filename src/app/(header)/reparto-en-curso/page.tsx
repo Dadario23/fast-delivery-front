e4bc374abf5ffdfe69fc710a18051e48f7140955
hernan789 <hernanlduarte@hotmail.com>
2024-01-22 13:30:45 -0300

    agrego vista reparto en curso

--- src/app/(header)/reparto-en-curso/page.tsx
+++ src/app/(header)/reparto-en-curso/page.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
+import RepartoEnCurso from "components/RepartoEnCurso";
+const RepartoEnCursoPage: React.FC = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-[#3d1df3]">
+      <RepartoEnCurso />
+    </div>
+  );
+};
 
-const page = () => {
-	return <div>Reparto en curso</div>
-}
-
-export default page
+export default RepartoEnCursoPage;
