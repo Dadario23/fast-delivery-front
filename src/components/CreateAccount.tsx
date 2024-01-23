6ff86c9f58d1e51165524055d4ed57638275eec1
hernan789 <hernanlduarte@hotmail.com>
2024-01-23 13:16:14 -0300

    modifico navbar y register

--- src/components/CreateAccount.tsx
+++ src/components/CreateAccount.tsx
@@ -5,8 +5,8 @@ import CameraIcon from "assets/CameraIcon/camera-icon";
 import BackIcon from "assets/BackIcon/back-icon";
 const CreateAccountForm: React.FC = () => {
   return (
-    <div className="flex items-center justify-center flex-wrap  rounded-xl mx-[30px] mb-[60px] bg-[#C7FFB1] relative ">
-      <div className="w-full h-[55px] pl-0 flex items-center font-bold rounded-t-xl bg-[#C7FFB1] z-10 ">
+    <div className="flex items-center justify-center flex-wrap  rounded-xl mx-[30px] mt-[10px] mb-[60px] bg-[#C7FFB1] relative ">
+      <div className="w-full h-[50px] pl-0 flex items-center font-bold rounded-t-xl bg-[#C7FFB1] z-10 ">
         <div className="ml-3">
           <BackIcon />
         </div>
@@ -14,7 +14,7 @@ const CreateAccountForm: React.FC = () => {
           Creá tu cuenta
         </h2>
       </div>
-      <div className="bg-white rounded-xl z-20">
+      <div className="bg-white rounded-xl z-20 text-[85%]">
         <form className="w-[19rem] pl-5">
           <div className="w-[95px] h-[95px] mb-4 mt-5 ml-[88px] flex items-center justify-center border border-[#3d1df3] rounded-full">
             <CameraIcon />
@@ -57,10 +57,10 @@ const CreateAccountForm: React.FC = () => {
           <button className="w-[270px] h-[30px] mt-4 mb-2 p-2 bg-[#00ea77] text-[#3d1df3] rounded-full pl-50 flex items-center justify-center ">
             Crear
           </button>
-          <h2 className="mb-2 flex items-center justify-center text-[#3d1df3]">
+          <h2 className="mb-2 flex items-center justify-center text-[#3d1df3] text-[12px]">
             ¿Ya tenés una cuenta?
           </h2>
-          <button className="w-[270px] h-[30px] mb-2 p-2 bg-white border border-[#00ea77] text-[#3d1df3] rounded-full pl-50 flex items-center justify-center ">
+          <button className="w-[270px] h-[30px] mb-2 p-2 bg-white border border-[#00ea77] text-[#3d1df3]  rounded-full pl-50 flex items-center justify-center ">
             Iniciar sesión
           </button>
         </form>
