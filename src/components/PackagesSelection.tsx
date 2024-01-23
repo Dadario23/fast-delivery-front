9880bc16c5f832786f25496346486aed9969b960
Valentinguardia <valentin.guardia@outlook.com>
2024-01-23 17:14:34 -0300

    obtener paquetes completo sin navbar terminada

--- src/components/PackagesSelection.tsx
+++ src/components/PackagesSelection.tsx
@@ -1,65 +1,7 @@
-import React from 'react'
-import styles from './PackagesSelection.module.css'
-import BackIcon from 'assets/BackIcon/back-icon'
-
-interface Package {
-  title: string
-}
-
-const packages: Package[] = [
-  {
-    title: 'Amenabar 2100, CABA'
-  },
-  {
-    title: 'Paquete 2',
-  },
-  {
-    title: 'Paquete 3',
-  },
-  {
-    title: 'Paquete 4',
-  },
-  {
-    title: 'Paquete 5',
-  },
-  {
-    title: 'Paquete 6',
-  },
-  {
-    title: 'Paquete 7',
-  },
-]
-const PackagesSelection = () => {
-  return (
-    <div className={styles.background}>
-      <div className={styles.TopContainer}>
-        <h1 className={styles.headerText3}>Obtener paquetes</h1>
-        <div className={styles.backIcon}><BackIcon /></div>
-      </div>
-        <div className={styles.container}>
-        <h1 className={styles.headerText2}>¿Cuántos paquetes repartirás hoy?</h1>
-        <hr className={styles.lineHorizontal} />
-          {packages.map((packageItem, index) => (
-            <div className={styles.packageCard} key={index}>
-              <div className={styles.checkbox}></div>
-              <div className={styles.packageTitle}>{packageItem.title}</div>
-              </div>
-          ))}
-        </div>
-        <button className={styles.centerBtn} >Iniciar jornada</button>
-    </div>
-  )
-}
-
-export default PackagesSelection
-
-
-
-
-
 // import React from 'react'
 // import styles from './PackagesSelection.module.css'
 // import BackIcon from 'assets/BackIcon/back-icon'
+// import Check from 'assets/Check/check'
 
 // interface Package {
 //   title: string
@@ -67,25 +9,25 @@ export default PackagesSelection
 
 // const packages: Package[] = [
 //   {
-//     title: 'Paquete 1'
+//     title: 'Amenabar 2100, CABA'
 //   },
 //   {
-//     title: 'Paquete 2',
+//     title: 'Av Carabobo y Rivadavia, CABA',
 //   },
 //   {
-//     title: 'Paquete 3',
+//     title: 'Melian 1242, CABA',
 //   },
 //   {
-//     title: 'Paquete 4',
+//     title: 'Castillo 670, CABA',
 //   },
 //   {
-//     title: 'Paquete 5',
+//     title: 'Gorriti 4595, CABA',
 //   },
 //   {
-//     title: 'Paquete 6',
+//     title: 'Av. Gral. Mosconi 1056, CABA',
 //   },
 //   {
-//     title: 'Paquete 7',
+//     title: 'Tacuari 1797, CABA',
 //   },
 // ]
 // const PackagesSelection = () => {
@@ -95,23 +37,137 @@ export default PackagesSelection
 //         <h1 className={styles.headerText3}>Obtener paquetes</h1>
 //         <div className={styles.backIcon}><BackIcon /></div>
 //       </div>
-//       <div className="container mx-auto">
 //         <div className={styles.container}>
-//           <h1 className={styles.headerText2}>¿Cuántos paquetes repartirás hoy?</h1>
-//           <hr className={styles.lineHorizontal} />
+//         <h1 className={styles.headerText2}>¿Cuántos paquetes repartirás hoy?</h1>
+//         <hr className={styles.lineHorizontal} />
 //           {packages.map((packageItem, index) => (
 //             <div className={styles.packageCard} key={index}>
-//               <div className={styles.packageTitle}>
-//                 <input type="checkbox" id={`package-checkbox-${index}`} />
-//                 <label htmlFor={`package-checkbox-${index}`}>{packageItem.title}</label>
+//               <div className={styles.checkbox}></div>
+//               <div> <Check/> </div>
+//               <div className={styles.packageTitle}>{packageItem.title}</div>
 //               </div>
-//             </div>
 //           ))}
 //         </div>
 //         <button className={styles.centerBtn} >Iniciar jornada</button>
-//       </div>
 //     </div>
 //   )
 // }
 
 // export default PackagesSelection
+
+"use client";
+import React from "react";
+import BackIcon from "assets/BackIcon/back-icon";
+import Check from "assets/Check/check";
+import "tailwindcss/tailwind.css";
+import { useState } from "react";
+
+interface Package {
+  title: string;
+}
+
+const packages: Package[] = [
+  {
+    title: "Amenabar 2100, CABA",
+  },
+  {
+    title: "Av Carabobo y Rivadavia, CABA",
+  },
+  {
+    title: "Melian 1242, CABA",
+  },
+  {
+    title: "Castillo 670, CABA",
+  },
+  {
+    title: "Gorriti 4595, CABA",
+  },
+  {
+    title: "Av. Gral. Mosconi 1056, CABA",
+  },
+  {
+    title: "Tacuari 1797, CABA",
+  },
+];
+
+const PackagesSelection: React.FC = () => {
+  const [selectedPackages, setSelectedPackages] = useState<number[]>([]);
+
+  const handleClick = (index: number) => {
+    setSelectedPackages((prevSelected) =>
+      prevSelected.includes(index)
+        ? prevSelected.filter((selectedIndex) => selectedIndex !== index)
+        : [...prevSelected, index]
+    );
+  };
+
+  // return (
+  //   <div className={styles.background}>
+  //     <div className={styles.TopContainer}>
+  //       <h1 className={styles.headerText3}>Obtener paquetes</h1>
+  //       <div className={styles.backIcon}><BackIcon /></div>
+  //     </div>
+  //     <div className={styles.container}>
+  //       <h1 className={styles.headerText2}>¿Cuántos paquetes repartirás hoy?</h1>
+  //       <hr className={styles.lineHorizontal} />
+  //       {packages.map((packageItem, index) => (
+  //         <div className={styles.packageCard} key={index}>
+  //           <div
+  //             className={styles.checkbox}
+  //             onClick={() => handleClick(index)}
+  //           >
+  //             {selectedPackages.includes(index) && <Check />}
+  //           </div>
+  //           <div className={styles.packageTitle}>{packageItem.title}</div>
+  //         </div>
+  //       ))}
+  //     </div>
+  //     <button className={styles.centerBtn}>Iniciar jornada</button>
+  //   </div>
+  // );
+
+  return (
+    <div className="flex items-center justify-center flex-wrap rounded-xl mx-[30px] bg-[#C7FFB1] mt-[30px] h-[74px] relative ">
+      <div className="w-full h-[55px] pl-0 flex items-center font-bold rounded-t-xl bg-[#C7FFB1] z-10 ">
+        <div className="ml-3">
+          <BackIcon />
+        </div>
+        <h2 className=" font-bold text-[#3d1df3] mr-10 flex-grow text-center">
+          Obtener paquetes
+        </h2>
+      </div>
+      <div className="bg-white rounded-xl w-[300px] h-[452px] z-20">
+        <h1 className="text-[#3d1df3] text-center text-[12px] mt-2 mb-2">
+          ¿Cuántos paquetes repartirás hoy?
+        </h1>
+        <hr className="border-t border-[#3d1df3] mb-4 w-[270px] h-[0.5px] ml-4" />
+        {packages.map((packageItem, index) => (
+          <div
+            className="relative border border-[#3d1df3] h-[46px] w-[270px] ml-[15px] mb-[10px] rounded-[10px]"
+            key={index}
+          >
+            <div
+              className="absolute border-[1px] border-[#3d1df3] w-[16px] h-[16px] rounded-[5px] ml-[10px] mt-[14px] "
+              onClick={() => handleClick(index)}
+            >
+              {selectedPackages.includes(index) && (
+                <div className="ml-[-1px] mt-[-1px]">
+                  <Check />
+                </div>
+              )}
+            </div>
+            <div className="text-[#3d1df3] text-[12px] ml-[50px] mt-[9px]">
+              {packageItem.title}
+            </div>
+            <div className="bg-[#3d1df3] absolute w-[1px] h-[70%] left-[37px] top-[0.5rem]"></div>
+          </div>
+        ))}
+      </div>
+      <button className="bg-[#00ea77] text-[14px] text-[#3d1df3] rounded-[15px] h-[30px] w-[16.5rem] mt-[10px]">
+        Iniciar jornada
+      </button>
+    </div>
+  );
+};
+
+export default PackagesSelection;
