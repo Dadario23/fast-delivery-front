cf687e36779457a9306abc4dcc17fc0773961b7e
Valentinguardia <valentin.guardia@outlook.com>
2024-01-23 22:08:20 -0300

    vista paquetes backoffice

---
+++ src/app/(header)/packages-office/page.tsx
@@ -0,0 +1,11 @@
+import PackagesOffice from "../../../components/Packages"
+
+const PackagesSelectionPage = () => {
+  return (
+    <div>
+      <PackagesOffice />
+    </div>
+  )
+}
+
+export default PackagesSelectionPage
