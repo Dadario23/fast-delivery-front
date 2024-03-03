d3a35840185b7a595ed275925b47a33fffa5ab0c
hernan789 <hernanlduarte@hotmail.com>
2024-03-03 14:18:14 -0300

    add service and update affidavits.ts

---
+++ src/services/dataAffidavit.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosResponse } from 'axios'
+// import { UserData } from "../types/userTypes";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL
+
+export const postAffidavit = async (declaration: object) => {
+	try {
+		const response: AxiosResponse = await axios.post(
+			`${API_URL}/api/users/affidavit`,
+			declaration,
+			{
+				withCredentials: true,
+			}
+		)
+
+		return response.data
+	} catch (error) {
+		console.error('Error al obtener los usuarios:', error)
+		throw error
+	}
+}
