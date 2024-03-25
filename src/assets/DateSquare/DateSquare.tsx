dfa42aeca0ace20c8903ac9e6ce85b954d5aea01
hernan789 <hernanlduarte@hotmail.com>
2024-03-25 16:29:04 -0300

    update delivery-drivers

--- src/assets/DateSquare/DateSquare.tsx
+++ src/assets/DateSquare/DateSquare.tsx
@@ -1,12 +1,14 @@
 type Props = { day: string; date: number };
 
 const DateSquare = ({ day, date }: Props) => {
-  return (
-    <div className="p-2 py-4lex flex-col w-[41px] h-[50px] rounded-[10px] border border-[#3d1df3] border-bold text-[#3d1df3] text-[5px] flex items-center justify-center  ">
-      {day}
-      <div className="font-bold text-[16px]">{`0${date}`}</div>
-    </div>
-  );
-};
+	const fullDate = `${date}`.padStart(2, '0')
 
-export default DateSquare;
+	return (
+		<div className="p-2 py-4lex flex-col w-[41px] h-[50px] rounded-[10px] border border-[#3d1df3] border-bold text-[#3d1df3] text-[5px] flex items-center justify-center  ">
+			{day}
+			<div className="font-bold text-[16px]">{fullDate}</div>
+		</div>
+	)
+}
+
+export default DateSquare
